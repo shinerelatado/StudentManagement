cefc81a7b80af3f24d47e1ed541a1081dddc8e19
shinerelatado <shinerelatado@gmail.com>
2025-07-15 15:23:23 +0800

    update sourcecode

---
+++ vendor/composer/index.php
@@ -0,0 +1,22 @@
+<?php
+include 'vendor/ autoload.php';
+
+use Create\App\Model\StudentModel;
+
+$Student = new Studends;
+$Student-> id = 262176381;
+$Student-> Fullname = "shanglia";
+$Student-> yearlevel = "secondyear";
+$Student-> course = "BSIT2";
+$Student-> section = "A";
+
+$Student->sdisplayInfo(); 
+
+
+
+
+     
+
+
+
+
